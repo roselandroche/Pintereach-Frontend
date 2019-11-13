eaf69978af318b062389b0e936190ee22bc285e6
spettigrew <sara.pettigrew5@gmail.com>
2019-11-13 15:42:36 -0700

    added action/reducer files. Installed redux

--- articles/src/index.js
+++ articles/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom"
+import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { reducer } from './reducer/articles';
+
 import './index.css';
 import App from './App';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <App />
+//     </Provider>,
+//     document.getElementById("root")
+// );
 
 ReactDOM.render(
     <BrowserRouter>
         <App />
-    </BrowserRouter>,
-    document.getElementById("root"),
-)
-
+    </BrowserRouter >,
+    document.getElementById('root'));
