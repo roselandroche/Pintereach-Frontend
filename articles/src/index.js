261872227891fc9ee207daba43d2db56529bc84e
spettigrew <sara.pettigrew5@gmail.com>
2019-11-13 17:46:38 -0700

    browserRouter set up in index.js

--- articles/src/index.js
+++ articles/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom"
 import { createStore, applyMiddleware } from 'redux';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { reducer } from './reducer/articles';
@@ -12,15 +12,17 @@ import App from './App';
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
-// ReactDOM.render(
-//     <Provider store={store}>
-//         <App />
-//     </Provider>,
-//     document.getElementById("root")
-// );
-
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter >,
-    document.getElementById('root'));
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter >
+    </Provider>,
+    document.getElementById("root")
+);
+
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <App />
+//     </BrowserRouter >,
+//     document.getElementById('root'));
