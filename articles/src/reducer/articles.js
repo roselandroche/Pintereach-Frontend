6dcb9274f5ef86d6eb7d3d0eef2b0aac4f9502d2
spettigrew <sara.pettigrew5@gmail.com>
2019-11-14 16:15:46 -0700

    fixed typo

--- articles/src/reducer/articles.js
+++ articles/src/reducer/articles.js
@@ -1,4 +1,3 @@
-import api from '../utils/api';
 
 import {
     ADD_TITLE, 
@@ -24,7 +23,7 @@ export function reducer(state = initialState, action) {
             case ARTICLE_SUCCESS:
                 return {
                     ...state, 
-                    title: action.payload,
+                    articles: action.payload,
                     isLoading: false
                 };
                 case ARTICLE_ERROR:
@@ -42,3 +41,14 @@ export function reducer(state = initialState, action) {
                         return state;
     }
 }
+
+// use useFetch custom hook.
+// {
+//   "id": 1,
+//   "title": "Updated A test Title",
+//   "summary": "Updated This is a little summary about the article..",
+//   "link": "https://www.britannica.com/topic/Pokemon-electronic-game",
+//   "image": "https://cdn.britannica.com/s:700x450/70/122270-004-0564DF2A.jpg",
+//   "user_id": 1,
+//   "category": "Updated Test Article"
+// }
