63c76b8d5309e848658b4cfd136bc5124759f707
brianetaveras <taveras574@gmail.com>
2019-11-14 19:35:20 -0500

    Edited PinnedArticles component and added category taf

--- articles/src/components/PinnedArticles.jsx
+++ articles/src/components/PinnedArticles.jsx
@@ -8,8 +8,15 @@ export default function PinnedArticles() {
       <div className="title">Pinned</div>
       <div className="articles">
           <div className="card">
-              
+              <div className="card-image">
+                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Edward_Snowden_-_Permanent_Record_%28cover%29.jpg/220px-Edward_Snowden_-_Permanent_Record_%28cover%29.jpg" alt=""/>
+              </div>
+              <div className="card-content">
+                  <div className="title">Permanent Record</div>
+                  <div className="category">Book</div>
+              </div>
           </div>
+
       </div>
     </div>
   );
