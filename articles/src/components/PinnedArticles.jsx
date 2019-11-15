f56eadeba0bf4257c6f6fb1f9a2a6f933d0af577
brianetaveras <taveras574@gmail.com>
2019-11-14 19:07:00 -0500

    Created DashboardNav component and Pinned Articles component

---
+++ articles/src/components/PinnedArticles.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+
+export default function PinnedArticles() {
+ 
+  return (
+    <div className="pinned-articles">
+      <div className="title">Pinned</div>
+      <div className="articles">
+          <div className="card">
+              
+          </div>
+      </div>
+    </div>
+  );
+}
