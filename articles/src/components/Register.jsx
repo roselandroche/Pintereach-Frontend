ebb098ffb0bb366488eb8597513e018e192150f6
brianetaveras <taveras574@gmail.com>
2019-11-14 21:30:43 -0500

    Added style to buttons and form fields

--- articles/src/components/Register.jsx
+++ articles/src/components/Register.jsx
@@ -2,30 +2,30 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from 'yup';
 
-const Register = ({ values, handleChange, touched, errors }) => {
+const Register = ({ values, touched, errors }) => {
   return (
     <div className="register">
       <div className="illustration">
-        <img width="300px" src="img/register-illustration.svg" alt="" />
+        <img width="300px" src="img/register-illustration.svg" alt="lighthouse illustration" />
       </div>
       <Form>
         <Field
+        className="styled-input"
           name="email"
           value={values.email}
           placeholder="email"
           type="text"
-          onChange={handleChange}
         />
         {touched.email && errors.email && ( <p className="error">{errors.email}</p>)}
         <Field
+        className="styled-input"
           name="password"
           value={values.password}
           placeholder="password"
           type="password"
-          onChange={handleChange}
           />
           {touched.password && errors.password && ( <p className="error">{errors.password}</p>)}
-        <button type="submit">Sign up</button>
+        <button className="primary-button" type="submit">Sign up</button>
       </Form>
     </div>
   );
