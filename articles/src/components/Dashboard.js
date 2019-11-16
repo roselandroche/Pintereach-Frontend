7888b2e49f5f0d73110c358c7d8748f111e7f153
spettigrew <sara.pettigrew5@gmail.com>
2019-11-15 17:12:51 -0700

    idea for category included in form. dashboard.js

--- articles/src/components/Dashboard.js
+++ articles/src/components/Dashboard.js
@@ -11,3 +11,29 @@ function ArticleList() {
 }
 
 export default ArticleList
+
+
+{/* <Form>
+
+{ touched.category && errors.category && <p className='error'>{errors.category}</p> }
+<Field component="select" name="category">
+    <option value="" disabled>Select Category:</option>
+    <option value="news">News</option>
+    <option value="entertainment">Entertainment</option>
+    <option value="sports">Sports</option>
+    <option value="money">Money</option>
+    <option value="lifestyle">Lifestyle</option>
+    <option value="food">Food and Drink</option>
+    <option value="travel">Travel</option>
+    <option value="other">Other</option>
+</Field>
+</Form>
+
+
+export default withFormik({
+        mapPropsToValues: (values) => {
+           return { 
+               category: values.category || ''
+           }
+        },
+} */}
