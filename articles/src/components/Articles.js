996764d400d5abca1f7f7e30428546d24bebadf7
rlom96 <rlom96@gmail.com>
2019-11-18 17:06:12 -0600

    added dummy data object, added buttons

--- articles/src/components/Articles.js
+++ articles/src/components/Articles.js
@@ -2,8 +2,32 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { addArticle } from "../action/articles";
 import Navbar from "./DashboardNav";
-// import articleImage from "../images/articleImage.jpeg";
 import api from "../utils/api";
+
+const data = [
+  {
+    id: 1,
+    title: "Permanent Record",
+    summary:
+      "Pokémon, electronic game series from Nintendo that debuted in Japan in 1995 and later became wildly popular in the United States. The series, originally produced for the company's Game Boy line of handheld consoles, was introduced in 1998 to the United States with two titles, known to fans as Red and Blue.",
+    link: "https://www.britannica.com/topic/Pokemon-electronic-game",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51z1ZaEn6sL._SX327_BO1,204,203,200_.jpg",
+    user_id: 1,
+    category: "Article"
+  },
+  {
+    id: 2,
+    title: "The Design Hustle",
+    summary:
+      "Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne",
+    link: "https://en.wikipedia.org/wiki/Dota_2",
+    image:
+      "https://cdn.cp.adobe.io/content/2/rendition/2fbc681c-bb10-4ddd-9d27-ae0764a88610/version/0/format/jpg/dimension/width/size/200",
+    user_id: 2,
+    category: "Article"
+  }
+];
 
 function Articles(props) {
   useEffect(() => {
@@ -14,14 +38,18 @@ function Articles(props) {
     <div>
       <Navbar />
       <div className="article-container">
-        <div className="article-image">{/* <img src={articleImage} /> */}</div>
+        <div className="article-image">
+          <img src={data[0].image} alt="" />
+        </div>
         <div className="article-information">
-          <h1> Testing!!!!</h1>
-          <h1> Testing!!! with sudo chwon</h1>
-          <h3>Permanent Record</h3>
-          <p>Permanent Recordby Edward Snowden</p>
+          <h3>{data[0].title}</h3>
+          <p>By Edward Snowden</p>
           <button>Book</button>
-          <p>Permanent Record is a Bio </p>
+          <p>{data[0].summary} </p>
+          <div className="article-buttons">
+            <button className="primary-button">Read</button>
+            <button className="primary-button">Add to Pinned</button>
+          </div>
         </div>
       </div>
     </div>
