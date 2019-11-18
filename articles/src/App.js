1c99c04664187f44cc9474622b9c97804d731361
rlom96 <rlom96@gmail.com>
2019-11-17 18:46:33 -0600

    initial changes, added content

--- articles/src/App.js
+++ articles/src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Link, Route, withRouter } from 'react-router-dom';
-import { getToken } from './utils/api';
-import ProtectedRoute from './components/ProtectedRoute';
-import Login from './components/Login';
-import Logout from './components/Logout';
-import Dashboard from './components/Dashboard';
-import Articles from './components/Articles';
-import UpdateArticle from './components/UpdateArticle';
-import Register from './components/Register'
+import React from "react";
+import { Link, Route, withRouter } from "react-router-dom";
+import { getToken } from "./utils/api";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Dashboard from "./components/Dashboard";
+import Articles from "./components/Articles";
+import UpdateArticle from "./components/UpdateArticle";
+import Register from "./components/Register";
 
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  const signedIn = getToken()
+  const signedIn = getToken();
 
   return (
     <div className="wrapper">
@@ -23,23 +22,21 @@ function App() {
 
           {!signedIn && <Link to="/login">Login</Link>}
           {signedIn && <Link to="/logout">Logout</Link>}
-          <Link to='/dashboard'>Dashboard</Link>
-          <Link to='/articles'>Articles</Link>
-          <Link to='/updatearticle'>Update Article</Link>
-          </nav>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/articles">Articles</Link>
+          <Link to="/updatearticle">Update Article</Link>
+        </nav>
 
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        <ProtectedRoute exact path='/dashboard' component={Dashboard} />
-        <ProtectedRoute exact path='/articles' component={Articles} />
-        <ProtectedRoute exact path='/updatearticle' component={UpdateArticle} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+        <ProtectedRoute exact path="/articles" component={Articles} />
+        <ProtectedRoute exact path="/updatearticle" component={UpdateArticle} />
         <ProtectedRoute exact path="/logout" component={Logout} />
-        <Articles />
+        {/* <Articles /> */}
       </div>
     </div>
   );
 }
 
 export default withRouter(App);
-
-
--- articles/src/components/Articles.js
+++ articles/src/components/Articles.js
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import { addArticle } from '../action/articles';
-
-import api from '../utils/api';
-
+import { addArticle } from "../action/articles";
+import Navbar from "./DashboardNav";
+// import articleImage from "../images/articleImage.jpeg";
+import api from "../utils/api";
 
 function Articles(props) {
+  useEffect(() => {
+    props.addArticle();
+  }, []);
 
-    useEffect(() => {
-        props.addArticle();
-    }, []);
-
-
-    return (
-        <div>
-            
+  return (
+    <div>
+      <Navbar />
+      <div className="article-container">
+        <div className="article-image">{/* <img src={articleImage} /> */}</div>
+        <div className="article-information">
+          <h3>Permanent Record</h3>
+          <p>Permanent Recordby Edward Snowden</p>
+          <button>Book</button>
+          <p>Permanent Record is a Bio </p>
         </div>
+      </div>
+    </div>
 
-        //  {props.isTitleLoading ? (
-        //     <div className='spinner' />
-        // )}
-    )
+    //  {props.isTitleLoading ? (
+    //     <div className='spinner' />
+    // )}
+  );
 }
 
 function mapStateToProps(state) {
-    return {
-        isTitleLoading: state.title.isLoading,
-    }
+  return {
+    isTitleLoading: state.title.isLoading
+  };
 }
 
 const mapDispatchToProps = {
-    addArticle
+  addArticle
 };
 
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Articles);
+export default connect(mapStateToProps, mapDispatchToProps)(Articles);
