6f579c7d3a12cd035012abba9761a22912735081
spettigrew <sara.pettigrew5@gmail.com>
2019-11-20 15:55:30 -0700

    added delete function. Update & Articles = error

--- articles/src/components/UpdateArticle.js
+++ articles/src/components/UpdateArticle.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { updateArticle } from '../action/update';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import api from '../utils/api'
 
-function UpdateArticle(props) {
+function UpdateArticle({ props, refresh, article }) {
     const [updateArticle, setUpdatedArticle] = useState({
         id: Date.now(),
         title: "",
@@ -22,7 +23,7 @@ function UpdateArticle(props) {
 
     const handleSubmit = event => {
         event.preventDefault()
-        props.updateArticle(updateArticle)
+        // props.updateArticle(updateArticle)
         setUpdatedArticle({
             id: Date.now(),
             title: "",
@@ -34,7 +35,21 @@ function UpdateArticle(props) {
         })
     }
 
+    const deleteArticle = (article) => {
+        if (window.confirm('Delete this article?')) {
+            api().delete('/api/articles/:id')
+            .then(refresh())
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+
     return (
+        <>
         <div> 
             <form onSubmit={handleSubmit}>
                 <label>Update Article</label>
@@ -73,9 +88,19 @@ function UpdateArticle(props) {
                     value={updateArticle.category}
                     onChange={handleChange}
                 />
+
+                <span>
+                    <span className="delete" onClick={e => {
+                        e.stopPropagation();
+                        deleteArticle(article)
+                    }
+                }>
+                </span>
+                </span>
                 <button type='submit'>Update</button>
             </form>
         </div>
+        </>
     )
 }
 
